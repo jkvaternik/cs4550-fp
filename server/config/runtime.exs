426c4c0c2a7f1584d421f0172c89294e9042a2ad
jimphowe <howe.ha@husky.neu.edu>
2021-04-01 16:22:48 -0400

    Ueberauth implementation

---
+++ server/config/runtime.exs
@@ -0,0 +1,28 @@
+import Config
+
+config :ueberauth, Ueberauth.Strategy.Spotify.OAuth,
+       client_id: "006d7532893548a89635c04a92dd1fe6",
+       client_secret: "5494091e5f3e42038eb1853784834cbb",
+       redirect_uri: "http://localhost:4000/auth/spotify/callback"
+
+case config_env() do
+  :prod ->
+    secret_key_base =
+      System.get_env("SECRET_KEY_BASE") ||
+        raise """
+        environment variable SECRET_KEY_BASE is missing.
+        You can generate one by calling: mix phx.gen.secret
+        """
+
+    config :rhapsody, RhapsodyWeb.Endpoint,
+           http: [
+             port: String.to_integer(System.get_env("PORT") || "4000"),
+             transport_options: [socket_opts: [:inet6]]
+           ],
+           url: [scheme: "https", host: "rhapsody.herokuapp.com", port: 443],
+           force_ssl: [rewrite_on: [:x_forwarded_proto]],
+           secret_key_base: secret_key_base
+
+  _ ->
+    nil
+end
--- server/lib/rhapsody_web/controllers/page_controller.ex
+++ server/lib/rhapsody_web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule RhapsodyWeb.PageController do
+  @moduledoc """
+  Static page controller
+  """
+
   use RhapsodyWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", current_user: get_session(conn, :current_user))
   end
 end
